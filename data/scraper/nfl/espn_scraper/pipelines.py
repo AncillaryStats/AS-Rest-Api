2c7d23a7b5335c6d19cabcc18ff2fb958700731b
Ansel Rosenberg <ansel01@gmail.com>
2015-09-14 23:56:24 -0700

    Update nfl team info pipeline to only add new teams

--- data/scraper/nfl/espn_scraper/pipelines.py
+++ data/scraper/nfl/espn_scraper/pipelines.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 from models import NFL_Team_2015, db_connect, create_tables
 
 class NFL_Team_2015_Pipeline(object):
@@ -11,19 +12,26 @@ class NFL_Team_2015_Pipeline(object):
         create_tables(engine)
         self.Session = sessionmaker(bind=engine)
 
+
+
     def process_item(self, item, spider):
         """This method is called for every item pipeline component."""
         session = self.Session()
         team = NFL_Team_2015(**item)
 
-        try:
-            session.add(team)
-            session.commit()
-        except:
-            session.rollback()
-            print('error saving item!')
-            raise
-        finally:
-            session.close()
+        # Add team if it does not exist yet
+        if not session.query(NFL_Team_2015).filter(NFL_Team_2015.name == item['name']).count():
+            print('new team found!')
+            try:
+                session.add(team)
+                session.commit()
+            except:
+                session.rollback()
+                print('error saving item!')
+                raise
+            finally:
+                session.close()
 
-        return item
+            return item
+        else:
+            print('team already exists')
