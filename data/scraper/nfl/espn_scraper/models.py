e779418412be9723dd6a0e842ac452f53d67dd8e
Ansel Rosenberg <ansel01@gmail.com>
2015-09-15 12:58:42 -0700

    Add model for nfl player info

--- data/scraper/nfl/espn_scraper/models.py
+++ data/scraper/nfl/espn_scraper/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import relationship, backref
 
 import settings
 
@@ -23,3 +24,17 @@ class NFL_Team_2015(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     division = Column(String)
+
+
+class NFL_Player_2015(Base):
+    """2015 NFL players table"""
+    __tablename__ = 'nfl_players_2015'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    number = Column(String)
+    team = Column(String)
+    position = Column(String)
+    # team_id = Column(Integer, ForeignKey('nfl_teams_2015.id'))
+
+    # team = relationship('Team', backref=backref('nfl_players_2015', order_by="position"))
