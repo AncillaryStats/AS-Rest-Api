55ef4707c329f0865d9bc76c5a859bf093dc2629
Ansel Rosenberg <ansel01@gmail.com>
2015-09-14 18:33:58 -0700

    Add basic settings for scrapy
    - Includes db connection, spiders and pipelines

---
+++ data/scraper/nfl/espn_scraper/settings.py
@@ -0,0 +1,23 @@
+
+SPIDER_MODULES = ['espn_scraper.spiders']
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'arosenberg',
+    'database': 'nfl_test_1'
+}
+
+ITEM_PIPELINES = {
+    'scraper_app.pipelines.NFL_Team_2015_Pipeline': 100
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# NOTE: AutoThrottle will honour the standard settings for concurrency and delay
+AUTOTHROTTLE_ENABLED=True
+# The initial download delay
+AUTOTHROTTLE_START_DELAY=3
+
+DOWNLOAD_DELAY=2
