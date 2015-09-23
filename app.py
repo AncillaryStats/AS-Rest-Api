ada6428829bc97197315de02282d19c92c27c64f
Ansel Rosenberg <ansel01@gmail.com>
2015-09-22 19:34:23 -0700

    Update app.py to handle rb games route requests

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from config import app, db
 
 from models.nfl_team_2015 import NFL_Team_2015
 from models.nfl_qb_game_2015 import NFL_QB_Game_2015
+from models.nfl_rb_game_2015 import NFL_RB_Game_2015
 
 
 def cleanup_queries(results):
@@ -15,14 +16,24 @@ def cleanup_queries(results):
         data.append(result.__dict__)
     return data
 
+# Serve static AngularJS files
 @app.route('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
 
+# Return qb game stats as json
 @app.route('/qbs', methods=['GET'])
 def get_qb_games():
     qb_games_2015 = NFL_QB_Game_2015.query.all()
     clean_games = cleanup_queries(qb_games_2015)
+    js = json.dumps(clean_games)
+    return js
+
+# Return rb game stats as json
+@app.route('/rbs', methods=['GET'])
+def get_rb_games():
+    rb_games_2015 = NFL_RB_Game_2015.query.all()
+    clean_games = cleanup_queries(rb_games_2015)
     js = json.dumps(clean_games)
     return js
 
