3eb91dee9fab069a6050b99833691a6a5a3ef515
Ansel Rosenberg <ansel01@gmail.com>
2015-09-24 22:07:11 -0700

    Change databases

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://arosenberg@localhost:5432/nfl_test_1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://arosenberg@localhost:5432/nfl_test_2'
 db = SQLAlchemy(app)
