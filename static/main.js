83cf532fe2ed4d497e48fcdea178cd28989706ad
Ansel Rosenberg <ansel01@gmail.com>
2015-09-24 22:52:15 -0700

    Clean up static/main.js

--- static/main.js
+++ static/main.js
@@ -2,86 +2,33 @@
 
   'use strict';
 
-  angular.module('SportsStats', [])
+  angular.module('SportsStats', [
+    'ui.router',
+    'n3-line-chart'
+    ])
 
-  .controller('NflQbCtrl', ['$http', '$q', function($http, $q) {
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-    var vm = this;
+    $urlRouterProvider.otherwise('/leader-tables');
 
-    vm.passingTdsLeaders = [];
-    vm.passingYardsLeaders = [];
-    vm.qbrLeaders = [];
+    $stateProvider
+    // .state('home', {
+    //   url: '/', 
+    //     templateUrl: '',
+    //     controller: ''
+    // })
+    .state('leader-tables', {
+      url: '/leader-tables',
+        templateUrl: 'static/leader-tables/leader-tables.html',
+        controller: 'LeaderTablesCtrl as tables'
+    })
+  }])
 
-    getQbGames()
+  .run(function($rootScope, $templateCache) {
+     $rootScope.$on('$viewContentLoaded', function() {
+        $templateCache.removeAll();
+     });
+  });
 
-    function getQbGames() {
-       $http.get('/qbs')
-       .then(function(response) {
-          var stats = [];
-          response.data.forEach(function(stat) {
-            if (stat.is_season_totals) {
-              stats.push(stat);
-            }
-          });
-
-          var passingTdLeaders = _.sortBy(stats, 'pass_tds').reverse();
-          var passingYardsLeaders = _.sortBy(stats, 'pass_yards').reverse();
-          var qbrLeaders = _.sortBy(stats, 'qb_rating').reverse();
-        
-          vm.passingTdsLeaders = _.first(passingTdLeaders, 10);
-          vm.passingYardsLeaders = _.first(passingYardsLeaders, 10);
-          vm.qbrLeaders = _.first(qbrLeaders, 10);
-          console.log(vm.qbrLeaders);
-          console.dir(response.data)
-
-       }, function(error) {
-          console.log('Error:', error);
-       })
-    }
-  }
-
-  ])
-
-  .controller('NflRbCtrl', ['$http', function($http) {
-
-    var vm = this;
-
-    vm.totalTdsLeaders = [];
-    vm.totalYardsLeaders = [];
-    vm.totalTouchesLeaders = [];
-
-    getRbGames();
-
-    function getRbGames() {
-      $http.get('/rbs')
-      .then(function(response) {
-        var stats = []
-        response.data.forEach(function(stat) {
-          if (stat.is_season_totals) {
-            stat.total_tds = stat.rush_tds + stat.rec_tds;
-            stat.total_yards = stat.rush_yards + stat.rec_yards;
-            stat.total_touches = stat.rush_attempts + stat.receptions;
-            stats.push(stat);
-          }
-        });
-
-        var totalTdsLeaders = _.sortBy(stats, 'total_tds').reverse()
-        var totalYardsLeaders = _.sortBy(stats, 'total_yards').reverse()
-        var totalTouchesLeaders = _.sortBy(stats, 'total_touches').reverse()
-
-        vm.totalTdsLeaders = _.first(totalTdsLeaders, 10);
-        vm.totalYardsLeaders = _.first(totalYardsLeaders, 10);
-        vm.totalTouchesLeaders = _.first(totalTouchesLeaders, 10)
-
-
-
-        console.dir(stats)
-      }, function(error) {
-        console.log('Error:', error);
-      })
-    }
-
-  }
-  ])
 
 })();
