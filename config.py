55dd8ff6cf6264cc2d3aab148472ccf5c00429ed
Ansel Rosenberg <ansel01@gmail.com>
2015-09-28 01:11:34 -0700

    Attempt db connection fix

--- config.py
+++ config.py
@@ -2,13 +2,12 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this needs to be changed soon'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -24,9 +23,8 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
 
-print os.environ['APP_SETTINGS']
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://arosenberg@localhost:5432/nfl_test_2'
+print(os.environ['DATABASE_URL'])
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://arosenberg@localhost:5432/nfl_test_2'
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
