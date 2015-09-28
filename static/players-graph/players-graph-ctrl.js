449323f2ac44ae47ecf21c29f46e31cdfe67b0fa
Ansel Rosenberg <ansel01@gmail.com>
2015-09-27 20:55:55 -0700

    Add graph view with graph info factory

---
+++ static/players-graph/players-graph-ctrl.js
@@ -0,0 +1,190 @@
+(function() {
+
+  'use strict';
+
+  angular
+  .module('SportsStats')
+  .controller('PlayersGraphCtrl', PlayersGraphCtrl)
+
+  PlayersGraphCtrl.$inject = ['$http', '$q', '$scope', 'PositionStats', 'PlayerInfo', '$rootScope', 'GraphInfo'];
+
+  function PlayersGraphCtrl($http, $q, $scope, PositionStats, PlayerInfo, $rootScope, GraphInfo) {
+
+    var position = '';
+    var players = [];
+    var games = [];
+    var stat;
+
+    var posMap = {
+      'qbs': PositionStats.getQbGames,
+      'rbs': PositionStats.getRbGames,
+      'wrs': PositionStats.getWrGames,
+      'tes': PositionStats.getTeGames,
+    }
+
+    $rootScope.$on('updateGraph', function() {
+      var gameData = []
+      var retrieveGames = posMap[GraphInfo.position]
+      retrieveGames()
+      .then(function(games) {
+
+        games = PositionStats[GraphInfo.position];
+  
+        // Get games of first five selected by players and sort by date
+        // Add to object with player's name and push to dataset to be show in graph
+        _.each(_.first(GraphInfo.players, 5), function(player) {
+          var playerGames = {
+            name: player,
+            games: _.where(games, { player_name: player })
+          }
+          playerGames.games = _.sortBy(playerGames.games, function (game) {
+            return game.date;
+          })
+          gameData.push(playerGames);
+        })
+
+        for (var i = 0; i < gameData.length; i++) {
+
+          var convertedCat = GraphInfo.config[GraphInfo.category].cat;
+          var values = _.pluck(gameData[i].games, convertedCat)
+          data.datasets[i].label = gameData[i].name;
+          data.datasets[i].data = values;
+        }
+
+        // Set scale options according to GraphInfo config for each cat
+        options.scaleSteps = GraphInfo.config[GraphInfo.category].scaleSteps
+        options.scaleStepWidth = GraphInfo.config[GraphInfo.category].scaleStepWidth
+
+        // Redraw chart
+        myLineChart.destroy()
+        myLineChart = new Chart(ctx).Line(data, options);
+
+      })
+
+    })
+
+
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var data = {
+      labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'],
+      // datasets: [
+      // ]
+
+      datasets: [
+          {
+              label: 'My First dataset',
+              fillColor: 'rgba(220,220,220,0.2)',
+              strokeColor: 'rgba(220,220,220,1)',
+              pointColor: 'rgba(220,220,220,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(220,220,220,1)',
+              data: []
+          },
+          {
+              label: 'My Second dataset',
+              fillColor: 'rgba(151,187,205,0.2)',
+              strokeColor: 'rgba(151,187,205,1)',
+              pointColor: 'rgba(151,187,205,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151,187,205,1)',
+              data: []
+          },
+          {
+              label: 'My third dataset',
+              fillColor: 'rgba(151,187,205,0.2)',
+              strokeColor: 'rgba(151,187,205,1)',
+              pointColor: 'rgba(151,187,205,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151,187,205,1)',
+              data: []
+          },
+          {
+              label: 'My third dataset',
+              fillColor: 'rgba(151,187,205,0.2)',
+              strokeColor: 'rgba(151,187,205,1)',
+              pointColor: 'rgba(151,187,205,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151,187,205,1)',
+              data: []
+          },
+          {
+              label: 'My third dataset',
+              fillColor: 'rgba(151,187,205,0.2)',
+              strokeColor: 'rgba(151,187,205,1)',
+              pointColor: 'rgba(151,187,205,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151,187,205,1)',
+              data: []
+          }
+      ]
+    };
+
+    var options = {
+
+        // Manually set scales
+        scaleOverride: true,
+        scaleSteps: 10,
+        scaleStepWidth: 1,
+        scaleStartValue: 0,
+
+        multiTooltipTemplate: "<%=datasetLabel%> : <%= value %>",
+
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor : 'rgba(0,0,0,.05)',
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+
+        //Boolean - Whether the line is curved between points
+        bezierCurve : false,
+
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension : 0.4,
+
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 4,
+
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
+
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius : 20,
+
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
+
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : false,
+
+        //String - A legend template
+        legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+
+    };
+
+    var myLineChart = new Chart(ctx).Line(data, options);
+
+  }
+
+})()
---
+++ static/players-graph/services/graph-info.js
@@ -0,0 +1,130 @@
+(function () {
+
+  angular
+    .module('SportsStats')
+    .factory('GraphInfo', GraphInfo);
+
+  GraphInfo.$inject = ['$http', '$q'];
+
+  function GraphInfo($http, $q){
+
+    var instance = {
+      // get: get,
+      players: [],
+      category: '',
+      position: '',
+      games: [],
+      statMap: '',
+      config: {
+        'Pass Attempts': {
+          cat: 'pass_attempts',
+          scaleSteps: 10,
+          scaleStepWidth: 10,
+        },
+        'Pass Completions': {
+          cat: 'pass_completions',
+          scaleSteps: 10,
+          scaleStepWidth: 10,
+        },
+        'Pass Yards': {
+          cat: 'pass_yards',
+          scaleSteps: 10,
+          scaleStepWidth: 70
+        },
+        'Completion %': {
+          cat: 'comp_percentage',
+          scaleSteps: 10,
+          scaleStepWidth: 10
+        },
+        'Average Yards/Pass': {
+          cat: 'avg_yards_per_pass',
+          scaleSteps: 10,
+          scaleStepWidth: 3
+        },
+        'Pass TDs': {
+          cat: 'pass_tds',
+          scaleSteps: 10,
+          scaleStepWidth: 1
+        },
+        'Interceptions': {
+          cat: 'interceptions',
+          scaleSteps: 10,
+          scaleStepWidth: 1
+        },
+        'QBR': {
+          cat: 'qb_rating',
+          scaleSteps: 10,
+          scaleStepWidth: 10
+        },
+        'Passer Rating': {
+          cat: 'passer_rating',
+          scaleSteps: 10,
+          scaleStepWidth: 20
+        },
+        'Rush Attempts': {
+          cat: 'rush_attempts',
+          scaleSteps: 10,
+          scaleStepWidth: 5
+        },
+        'Rush Yards': {
+          cat: 'rush_yards',
+          scaleSteps: 10,
+          scaleStepWidth: 25
+        },
+        'Average Yards/Rush': {
+          cat: 'avg_yards_per_rush',
+          scaleSteps: 10,
+          scaleStepWidth: 5
+        },
+        'Rush TDs': {
+          cat: 'rush_tds',
+          scaleSteps: 10,
+          scaleStepWidth: 1
+        }
+      }
+    };
+
+    instance.config
+
+    instance.statMap = {
+      'Pass Attempts': 'pass_attempts',
+      'Pass Completions': 'pass_completions',
+      'Pass Yards': 'pass_yards',
+      'Completion %': 'com_percentage',
+      'Average Yards/Pass': 'avg_yards_per_pass',
+      'Pass TDs': 'pass_tds',
+      'Interceptions': 'interceptions',
+      'QBR': 'qb_rating',
+      'Passer Rating': 'passer_rating',
+      'Rush Attempts': 'rush_attempts',
+      'Rush Yards': 'rush_yards',
+      'Average Yards/Rush': 'avg_yards_per_rush',
+      'Rush TDs': 'rush_tds'
+    }
+
+    return instance;
+
+
+
+    // If players do not exist, retrieve
+    // Resolve
+    // function get() {
+    //   var def = $q.defer()
+    //   if (instance.games.length) {
+    //     def.resolve();
+    //   } else {
+    //     $http.get('/players')
+    //     .then(function(res) {
+    //       instance.players = res.data;
+    //       def.resolve();
+    //     }, function(err) {
+    //       def.reject(err)
+    //     })
+    //   }
+    //   return def.promise;
+    // }
+
+
+  }
+
+}());
