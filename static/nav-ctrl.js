3612d149f94d361308dfe8beb2691ca4715d5563
Ansel Rosenberg <ansel01@gmail.com>
2015-10-04 23:38:23 -0700

    Add player search from nav bar

--- static/nav-ctrl.js
+++ static/nav-ctrl.js
@@ -6,10 +6,26 @@
   .module('SportsStats')
   .controller('NavCtrl', NavCtrl)
 
-  NavCtrl.$inject = ['$http', '$q', '$scope', 'PlayerInfo', 'PositionStats', '$rootScope', 'GraphInfo'];
+  NavCtrl.$inject = ['$http', '$q', '$scope', 'PlayerInfo', '$state'];
 
-  function NavCtrl($http, $q, $scope, PlayerInfo, PositionStats, $rootScope, GraphInfo) {
+  function NavCtrl($http, $q, $scope, PlayerInfo, $state) {
+     
+    // Search players by name
+    $scope.getPlayers = function($query) {
+      var def = $q.defer()
+      var playerNames = _.pluck(PlayerInfo.players, 'name')
+      var filteredPlayers = _.filter(playerNames, function(player) {
+        return player.toLowerCase().indexOf($query.toLowerCase()) != -1;
+      });
+      def.resolve(filteredPlayers)
+      return def.promise;
+    }
 
+    $scope.goToPlayer = function(playerName, query, tag) {
+      // $state.go('player', { name: tag.text.split(' ')[0] + '%20' + tag.text.split(' ')[1]})
+
+      $state.go('player', { name: tag.text })
+    }
   }
 
 })();
