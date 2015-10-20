89324db8bedb1925fc6c52784fa9ab3b7d1066f0
Ansel Rosenberg <ansel01@gmail.com>
2015-10-20 01:18:13 -0700

    Update api with resources for all teams, team rosters, and player games

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask_restful import Api
 from app import app
 from resources.nfl.player import NFL_Player_2015
-from resources.nfl.team import NFL_Teams_2015
+from resources.nfl.team import NFL_Teams_2015, NFL_Team_2015
 
 api = Api(app)
 
-api.add_resource(NFL_Player_2015, '/api/player/<int:id>')
 api.add_resource(NFL_Teams_2015, '/api/teams')
+api.add_resource(NFL_Player_2015, '/api/player/<int:player_id>', endpoint='player_id')
+api.add_resource(NFL_Team_2015, '/api/team/<int:team_id>', endpoint='team_id')
+
