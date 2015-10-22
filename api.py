d71ebd183585e5d95b79f83b9be6fce737199764
Ansel Rosenberg <ansel01@gmail.com>
2015-10-21 18:28:47 -0700

    Update api url paths

--- api.py
+++ api.py
@@ -18,13 +18,13 @@ api.add_resource(NFL_Team_2015, '/api/nfl/team/<int:team_id>', endpoint='team_id
 
 # Private APIS
 
-api.add_resource(All_NFL_Players_2015, '/api/players')
-api.add_resource(t.QB_Totals_2015, '/api/totals/qbs')
-api.add_resource(t.RB_Totals_2015, '/api/totals/rbs')
-api.add_resource(t.WR_Totals_2015, '/api/totals/wrs')
-api.add_resource(t.TE_Totals_2015, '/api/totals/tes')
-api.add_resource(g.QB_Games_2015, '/api/games/qbs')
-api.add_resource(g.RB_Games_2015, '/api/games/rbs')
-api.add_resource(g.WR_Games_2015, '/api/games/wrs')
-api.add_resource(g.TE_Games_2015, '/api/games/tes')
-api.add_resource(trend.Trending_Players, '/api/trending')
+api.add_resource(All_NFL_Players_2015, '/api/nfl/players')
+api.add_resource(t.QB_Totals_2015, '/api/nfl/totals/qbs')
+api.add_resource(t.RB_Totals_2015, '/api/nfl/totals/rbs')
+api.add_resource(t.WR_Totals_2015, '/api/nfl/totals/wrs')
+api.add_resource(t.TE_Totals_2015, '/api/nfl/totals/tes')
+api.add_resource(g.QB_Games_2015, '/api/nfl/games/qbs')
+api.add_resource(g.RB_Games_2015, '/api/nfl/games/rbs')
+api.add_resource(g.WR_Games_2015, '/api/nfl/games/wrs')
+api.add_resource(g.TE_Games_2015, '/api/nfl/games/tes')
+api.add_resource(trend.Trending_Players, '/api/nfl/trending')
