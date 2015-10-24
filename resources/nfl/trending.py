f4eea0b2f7de6f5744b744278b8fe4317ee61bd3
Ansel Rosenberg <ansel01@gmail.com>
2015-10-24 15:30:38 -0700

    Attempt redis data retrieval fix

--- resources/nfl/trending.py
+++ resources/nfl/trending.py
@@ -2,13 +2,14 @@ from app import r
 from flask_restful import Resource, abort
 from flask_restful.utils import cors
 import utils
+import json
 
 
 class Trending_Players(Resource):
     def get(self):
         """Return trending NFL players (already in JSON format)"""
         try:
-            trending = r.get('trending_store')
+            trending = json.loads(r.get('trending_store'))
         except:
             abort(400, response={
                 'status': 400,
